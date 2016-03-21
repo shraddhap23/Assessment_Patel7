08fc2d4c1336d206ce18194c956cff676508a9f4
Shraddha Patel <spatelrb@gmail.com>
2016-03-21 10:58:46 -0400

    assessment 7

---
+++ assessment_patel7.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	function Fibonacci(n){
+
+	//need a function that takes an argument n and adds the previous return to it for the next return 
+	// i++ for increment 
+	// say n = 5, output would need to be 0,1,1,2,3,5
+	// fibonnaci (5) = fibonnaci(4)string + "fibonnaci(4)sum" so as to make a new string  
+	}
+
+});
